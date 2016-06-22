e33b1b4a00bf13a392fc5053f35c8c0e666eb8b8
wyqzl <wuyuqian@admindeiMac-35.local>
2016-06-22 17:04:04 +0800

    数组冒泡排序
    输入20个数，按照从小到大的顺序进行排序，运用for循环控制循环次数，把所有的数字都进行比较，保证数字没有遗漏。

--- array/main.swift
+++ array/main.swift
@@ -6,7 +6,23 @@
 //  Copyright © 2016年 吴玉倩. All rights reserved.
 //
 
-import Foundation
+import Cocoa
+var array = [1,56,976,3,76,89,2,39,80,99,78,543,987,35,57,16,66,88,99,100]
 
-print("Hello, World!")
 
+
+for i in 0 ..< array.count - 1 {
+    for j in 0 ..< array.count - 1 - i{
+        if array[j]>array[j + 1]{
+            var temp = array[j + 1]
+            array[j + 1] = array[j]
+            array[j] = temp
+            
+        }
+    }
+}
+  for item in array
+{
+    var ii = item
+    print(ii)
+}
